eafe1815f90b1cd35fbef68b4ed93bce1414119b
balamurali <balamuralipati@outlook.com>
2019-10-27 00:09:42 +0530

    initial commit

---
+++ src/test/java/com/springcloud/cloud/Eureka/service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.cloud.Eureka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
